660b27a033d7fa9ed0ae065b79a7ebb184110a57
Maria Eduarda Lustre <dudalustre35@gmail.com>
2025-09-01 19:04:56 -0300

    Implementada internacionalização da página de cadastro

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// Importe o componente que criamos
+import LanguageSelector from './components/LanguageSelector/LanguageSelector';
 
 import Cadastro from './components/Cadastro/Cadastro';
 import ConfiguracaoFila from './components/ConfiguracaoFila/ConfiguracaoFila';
@@ -12,16 +14,18 @@ import RedefinirSenha from './components/RedefinirSenha/RedefinirSenha';
 import Empresa from './components/Empresa/Empresa';
 import FilaLista from './components/ListarFilas/FilaLista';
 import GestaoFilaClientes from './components/GestaoFilaClientes/GestaoFilaClientes';
-// Novo componente para a tela de listagem de filas configuradas
-import FilasCadastradas from './components/FilasCadastradas/FilasCadastradas'; // Crie este arquivo
+import FilasCadastradas from './components/FilasCadastradas/FilasCadastradas';
 import Dashboard from './components/Dashboard/Dashboard';
-import Relatorio from './components/Relatorio/Relatorio'
+import Relatorio from './components/Relatorio/Relatorio';
 import PainelFilaExibicao from './components/PainelFilaExibicao/PainelFilaExibicao';
 
 
 function App() {
   return (
     <Router>
+      {/* Adicione o seletor de idioma aqui, fora de <Routes> */}
+      <LanguageSelector />
+      
       <Routes>
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/landing" element={<LandingPage />} />
@@ -29,20 +33,13 @@ function App() {
         <Route path="/esqueci-senha" element={<EsqueciSenha />} />
         <Route path="/redefinir-senha/:token" element={<RedefinirSenha />} />
         <Route path="/escolher-empresa" element={<Empresa />} />
-        {/* Rota para o Formulário de Configuração de Fila (cadastro e edição) */}
-        {/* O ':id?' indica que o ID é opcional. Sem ID = cadastro, Com ID = edição */}
-        <Route path="/configuracao/:id?" element={<ConfiguracaoFila />} />
-
-        {/* Nova rota para a tela de listagem de filas configuradas */}
-        {/* Este é o componente que vai exibir a tabela da imagem de referência */}
-        <Route path="/filas-cadastradas" element={<FilasCadastradas />} />
-
+        <Route path="/configuracao/:id?" element={<ConfiguracaoFila />} />
+        <Route path="/filas-cadastradas" element={<FilasCadastradas />} />
         <Route path="/filas" element={<FilaLista />} />
         <Route path="/gestao-fila/:idEmpresa/:dtMovto/:idFila" element={<GestaoFilaClientes />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/relatorio" element={<Relatorio />} />
         <Route path="/painel-fila/:idEmpresa/:dtMovto/:idFila" element={<PainelFilaExibicao />} />
-
 
         <Route
           path="/home"
